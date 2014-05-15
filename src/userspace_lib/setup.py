483e453a22a5633b42241a0242142dbc14f55a6e
Jason Kridner <jdk@ti.com>
2014-05-15 12:53:59 -0400

    Add setup.py

---
+++ src/userspace_lib/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pru_speak',
+      version='0.0',
+      description='BotSpeak interpreter for BeagleBone Black PRU-ICSS',
+      author='Deepak Karki',
+      author_email='deepak6k@gmail.com',
+      url='https://github.com/deepakkarki/pruspeak',
+      packages=['pru_speak']
+     )
