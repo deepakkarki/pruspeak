91d0910f68b9f449779a40ed00a79d73eb4e4b1b
Jason Kridner <jdk@ti.com>
2014-06-23 00:32:22 -0400

    Use built-in cape-universal

--- run-demo.sh
+++ run-demo.sh
@@ -1,8 +1,8 @@
-config-pin overlay cape-universaln
-rmmod uio_pruss
-cd src/driver && make load && cd ../..
-cd src/dts && make install && cd ../..
-cd src/pru-firmware && make install && cd ../..
+(cd src/driver; make load)
+(cd src/dts && make install)
+(cd src/pru-firmware && make install)
 config-pin overlay BB-PRUSPEAK
+echo 1 > /sys/devices/ocp.*/4a300000.prurproc/reset
+echo pru_firmware > /sys/devices/ocp.*/4a300000.prurproc/load
 config-pin p9.92 pruout
-
+python src/userspace_lib/client.py
--- src/userspace_lib/client.py
+++ src/userspace_lib/client.py
@@ -2,7 +2,7 @@ import pru_speak
 #import compiler
 
 botspeak_code	= \
-'''	SET DIO[4] , 1
+'''	SET DIO[4], 1
 	WAIT 1
 	SET DIO[4], 0
 	WAIT 1
