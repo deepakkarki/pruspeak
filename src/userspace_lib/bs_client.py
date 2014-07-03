878d5297e8530a061eff82cc5929a84b678c5fdf
deepakkarki <deepak6k@gmail.com>
2014-07-03 23:06:28 +0530

    terminal support for PRU Speak :D

--- src/userspace_lib/bs_client.py
+++ src/userspace_lib/bs_client.py
@@ -1,21 +1,39 @@
 import pru_speak
 import socket
+import sys 
+out = sys.stdout
+sentinel = ''
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 6060
 BUFFER_SIZE = pru_speak.PAGE_SIZE * 2
-MESSAGE = \
-'''     SET DIO[0] , 1
-        WAIT 1000
-        SET DIO[0], 0
-        WAIT 1000
-        GOTO 0'''
+
+def get_data():
+	out.write("ps>") #prompt the user
+	l = []
+	
+	for line in iter(raw_input, sentinel):
+		l.append(line)		#get the input
+		out.write("...")	
+	
+	return '\n'.join(l)	#return the data entered
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
-data = s.recv(BUFFER_SIZE)
-s.close()
 
-print "received data:", data
+while 1:
+	try:
+		msg = get_data()
+		print "sending message\n", msg, "\n"
+		s.send(msg)
+		data = s.recv(BUFFER_SIZE)
+		print "received data:", data
+
+	except Exception as e:
+		print e
+		print "Ending Connection"
+		s.close()
+		break		
+		
+
--- src/userspace_lib/bs_server.py
+++ src/userspace_lib/bs_server.py
@@ -1,29 +1,85 @@
 import pru_speak
 import socket
 
+def handle_data(bs_code):
+        '''
+        handles the BS commands which are part of the incoming packet
+        seperately takes care of single instructions and scripts
+        bs_code (str) : TCP packet data. The code block with BS instructions seperated by new lines.
+        '''
+        #print "bs_code.strip() " , bs_code.strip()
+        bs_code = bs_code.strip().split("\n")
+        bs_code = map(str.strip, bs_code)
+        print "bs_code \n", bs_code
+        # change logic either here or in compiler.py to make 
+        # all this more clean and efficient. but this is ok for now.
+
+        if "SCRIPT" in bs_code:
+                #download and keep the script
+                start = bs_code.index("SCRIPT")
+                try:
+                        end = bs_code.index("ENDSCRIPT")
+                        #get the script postion
+                        script_list = bs_code[start+1 : end]
+                        #remove it out of the original list of inst.
+                        bs_code = bs_code[:start] + bs_code[end+1 :]
+
+                        pru_speak.SCRIPT_CODE = '\n'.join(script_list)
+                        print "SCRIPT loaded\n",  pru_speak.SCRIPT_CODE + "\n ENDSCRIPT \n"
+                except Exception as e:
+			print "ENDSCRIPT NOT FOUND"
+                        return -1 #ENDSCRIPT not found - syntax error
+
+        for inst in bs_code :
+                if inst == "RUN": #have to move run to PRU, why simply put it here?
+                        if pru_speak.SCRIPT_CODE :
+                                #maybe abort the previous script before this?
+                                pru_speak.load(pru_speak.SCRIPT_CODE, trigger = True)
+                        else :
+                                return -1
+                else :
+                        pru_speak.single_instruction(inst)
+
+        return 1
+
+#Server on local host @ port 6060
 TCP_IP = '127.0.0.1'
 TCP_PORT = 6060
+
+#The max size upto which is recieved at one go
 BUFFER_SIZE = pru_speak.PAGE_SIZE * 2  # Normally 1024, but we want fast response
 
+#create a passive socket and bind it to 127.0.0.1:6060
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
+
+#Take max of one connection
 s.listen(1)
+print "PRU Speak server at ", TCP_IP + ":" + str(TCP_PORT)
+
+#connection is a one time affair - handles only one client
+conn, addr = s.accept()
+print 'Connection address:', addr
 
 while 1:
 	try:
-		conn, addr = s.accept()
-		print 'Connection address:', addr
+		#blocking wait for data from client
 		bs_code = conn.recv(BUFFER_SIZE)
-		if not bs_code: break
-		#print "received data:", bs_code
-		pru_speak.load(bs_code)
-		pru_speak.execute()
-		conn.send('ok, executing script')  # echo
-		conn.close()
-	
+		print "recieved\n", bs_code, "\n"
+		#print type(bs_code)
+		if not bs_code: break #why did I keep this? would I ever want to break out of the loop?
+		
+		#have to take care of return values later!
+		if handle_data(bs_code) == 1: #does all the single_instruction, load, execute etc
+			conn.send('ok\n')
+                        print "ok"
+		else:
+			conn.send('syntax error')
+                        print "syntax error"
+
 	except Exception as e:
-		print "error in connection"
-	
-	finally:
-		conn.close()
+		print "error : ", e
+		break
+
+conn.close()
 
