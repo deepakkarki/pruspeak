b9bbcc0250daf1fc9ed99592b0b7690e424b6036
deepakkarki <deepak6k@gmail.com>
2014-07-10 10:48:20 +0530

    added parsing support for more rules. Now to develop a code generator for the same

--- src/userspace_lib/bs_parse.py
+++ src/userspace_lib/bs_parse.py
@@ -6,7 +6,8 @@ Grammar for the parser :
 
 inst : SET val , val
 	| GET val
-	| IF  ( val COND val ) GOTO val
+	| IF  ( val cond val ) GOTO val
+	| WAIT val
 	| GOTO val 
 	| ADD val , val
 	| SUB val , val 
@@ -30,13 +31,51 @@ val : INT
 	
 arr : VAR [ INT ]
 	| VAR [ VAR ]
-	
+
+cond : GTE
+	| LTE	
+	| GT	
+	| LT	
+	| EQ	
+	| NEQ
 """
 
 def p_inst_SET(p):
 	'''inst : SET val ',' val'''
-	print "SET command -", " val1 : " + str(p[2]), " val2 : " + str(p[4])
+	print "SET command -", " val1 : ", p[2], " val2 : " , p[4]
+
+def p_inst_WAIT(p):
+	'''inst : WAIT val'''
+	print "WAIT command - val :", p[2]
 	
+def p_inst_GOTO(p):
+	'''inst : GOTO val'''
+	print "GOTO command - val :", p[2]
+
+def p_inst_IF(p):
+	'''inst : IF '(' val cond val ')' GOTO val'''
+	print "IF command - ", "val1 :", p[3], "val2 :", p[5], "cond :", p[4], "GOTO :", p[8]
+	
+def p_inst_ADD(p):
+	'''inst : ADD val ',' val'''
+	print "ADD command -", " val1 : ", p[2], " val2 : " , p[4]
+	
+def p_inst_SUB(p):
+	'''inst : SUB val ',' val'''
+	print "SUB command -", " val1 : ", p[2], " val2 : " , p[4]
+
+def p_inst_SCRIPT(p):
+	'''inst : SCRIPT'''
+	print 'SCRIPT'
+
+def p_inst_ENDSCRIPT(p):
+	'''inst : ENDSCRIPT'''
+	print 'ENDSCRIPT'
+
+def p_inst_HALT(p):
+	'''inst : HALT'''
+	print "HALT"
+
 def p_val_INT(p):
 	'''val : INT'''
 	#print p[1]
@@ -62,6 +101,15 @@ def p_arr_VAR2(p):
 	"""arr : VAR '[' VAR ']' """
 	#print "p_arr_VAR2 : p[1], [2]" ,  p[1], p[3]
 	p[0] = ( p[1], p[3])
+	
+def p_cond_ops(p):
+	'''cond : GTE
+		| LTE
+		| GT
+		| LT
+		| EQ
+		| NEQ'''
+	p[0] = p[1]
 
 # Error rule for syntax errors
 def p_error(p):
@@ -71,7 +119,18 @@ def p_error(p):
 
 # Build the parser
 parser = yacc.yacc()
-s = '''SET DIO[myvar], 1'''
-result = parser.parse(s)
+s = [ 
+	'SET DIO[myvar], 1', 
+	'IF (a > b) GOTO c' , 
+	'ADD myvar, 5', 
+	'SUB arr[4], another_var',
+	'GOTO 4',
+	'IF ( arr1[a] < arr2[b] ) GOTO arr3[c]',
+	'SCRIPT',
+	'ENDSCRIPT',
+	'GOTO arr3[4]'
+]
+for inst in s :
+	parser.parse(inst)
 #print result
 
