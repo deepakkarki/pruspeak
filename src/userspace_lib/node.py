157fcc55ca0a7503b2fdd25237a945ec676beca5
deepakkarki <deepak6k@gmail.com>
2014-07-11 01:47:40 +0530

    more getter/setters for cleaner interface

--- src/userspace_lib/node.py
+++ src/userspace_lib/node.py
@@ -15,4 +15,19 @@ class Value(object):
 		'''
 		return (self.type == 'ARR') and (type(self.val[1]) == str)
 		
+	def is_arr_const(self):
+		'''
+		checks if the value is a array indexed by a const
+		'''
+		return (self.type == 'ARR') and (type(self.val[1]) == int)
+		
+	def is_any_var(self):
+		'''
+		checks if the value is either ARR[const] or VAR
+		'''
+		return self.type == 'VAR' or self.is_arr_const()
+		
 	arr_var = property(is_arr_var, None)
+	arr_const =  property(is_arr_const, None)
+	any_var = property(is_any_var, None)
+	
