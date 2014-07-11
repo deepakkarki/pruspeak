393e24a30161f21525cc0db29a45be5d8be185f2
deepakkarki <deepak6k@gmail.com>
2014-07-12 01:31:37 +0530

    code generator working a good deal. almost all inst now supported

--- src/userspace_lib/bs_parse.py
+++ src/userspace_lib/bs_parse.py
@@ -9,14 +9,16 @@ pru_var_count = 0
 pru_vars = {
 		#e.g. "var1" : 0x02,
 		"var1" : 0x02,
-		"var2" : 0x03
+		"var2" : 0x03,
+		"var3" : 0x04
 }
 
 #does the array to (address, size) mapping.
 pru_arrs = {#add DIO, PWM etc. here
 		#e.g. "arr1" : (0x10, 5) 
 					#start, size
-		"arr1" : (0x10, 5) 
+		"arr1" : (0x10, 5) ,
+		"arr2" : (0x20, 5)
 }
 
 def BYTE(val, n):
@@ -46,7 +48,7 @@ def get_var(val):
 	'''
 	if val.arr_const:
 		#val1 is an const indexed array
-		return pru_arrs[val.val[0]][0] + val[1]
+		return pru_arrs[val.val[0]][0] + val.val[1]
 	else:
 		#val1 is an VAR
 		return pru_vars[val.val]
@@ -56,6 +58,8 @@ def byte_code_set(val1, val2):
 	encodes instruction of the form SET val1, val2
 	val1 is V/Arr ; val2 can be C/V/Arr(with const or var index)
 	'''
+	global pru_var_count
+	#opcodes used 16, 17, 18
 	OPCODE = 0x10
 	byte0, byte1, byte2 = 0, 0, 0
 	byte4, byte5, byte6, byte7 = 0, 0, 0, 0
@@ -69,29 +73,41 @@ def byte_code_set(val1, val2):
 		#x is an arr[var], y can be anything 
 			byte0 = pru_vars[val1.val[1]]
 			byte1 = pru_arrs[val1.val[0]][0]
+			byte2 |= 0b10 << 6
 			
 			if val2.type == 'INT':
 			#y is a Const
-				byte2 = 00 << 6
+				byte2 |= 0b00 << 4
 				byte4 = val2.val
 			
 			elif val2.any_var:
 			#y is a var
-				byte2 = 01 << 6
+				byte2 |= 0b01 << 4
 				byte4 = get_var(val2)
 			
 			else:
 			#y is arr[var]
-				byte2 = 3 <<6
+				byte2 |= 0b10 <<4
 				byte4 = pru_vars[val2.val[1]] #var
 				byte5 = pru_arrs[val2.val[0]][0] #arr
 				
 		else:
 		#y is arr[var], x is var/arr[const]
-			byte2 = 2 << 6
 			if val1.any_var:
-				byte0 = get_var(val1)
+				byte2 |= 0b01 << 4
 				
+				if val1.arr_const:
+				#val1 is an const indexed array
+					byte0 = pru_arrs[val1.val[0]][0] + val1.val[1]
+				else:
+				#val1 is an VAR
+					byte0 = pru_vars.get(val1.val, None)
+					if byte0 == None:
+						pru_vars[val1.val] = pru_var_count
+						byte0 = pru_var_count
+						pru_var_count += 1	
+
+			byte2 |= 0b10 << 4
 			byte4 = pru_vars[val2.val[1]]
 			byte5 = pru_arrs[val2.val[0]][0]
 		
@@ -101,7 +117,7 @@ def byte_code_set(val1, val2):
 	
 		if val1.arr_const:
 		#val1 is an const indexed array
-			byte2 = pru_arrs[val1.val[0]][0] + val1[1]
+			byte2 = pru_arrs[val1.val[0]][0] + val1.val[1]
 		else:
 		#val1 is an VAR
 			byte2 = pru_vars.get(val1.val, None)
@@ -120,7 +136,7 @@ def byte_code_set(val1, val2):
 			
 			if val2.arr_const:
 			#val1 is an const indexed array
-				byte0 = pru_arrs[val2.val[0]][0] + val2[1]
+				byte0 = pru_arrs[val2.val[0]][0] + val2.val[1]
 			else:
 			#val1 is an VAR
 				byte0 = pru_vars[val2.val]
@@ -134,6 +150,7 @@ def byte_code_set_r(val1, val2):
 	encodes instructions of the form SET DIO[a], arr[b]; 
 	where DIO can replaced by PWM, TMR, AI etc
 	'''
+	#opcodes used 1-15
 	OPCODE_SET = {
 				'DIO' 	: 0x01,
 				'PWM' 	: 0x04,
@@ -194,6 +211,250 @@ def byte_code_set_r(val1, val2):
 	#pack all the bytes
 	print OPCODE, byte2, byte1, byte0  #packed_bytes		
 				
+
+def byte_code_single_op(cmd, val):
+	'''
+	single operand instructions
+	e.g. WAIT, GOTO, GET
+	'''
+	#opcode 19
+	opcode_dict = {
+				'WAIT'	: 19,
+				'GOTO'	: 20,
+				'GET'	: 21
+	}
+	OPCODE = opcode_dict[cmd]
+	byte2, byte1, byte0 = 0, 0, 0
+
+	if val.type == 'INT':
+	#case1 INT
+		byte2 = 0
+		byte0 = val.val
+		
+	elif val.any_var:
+	#Var or Arr[Var]
+		byte2 = 0b01 << 6
+		byte0 = get_var(val)
+		
+	else:
+		byte2 = 0b10 << 6
+		byte0 = pru_vars[val.val[1]]
+		byte1 = pru_arrs[val.val[0]][0]
+	
+	byte3 = OPCODE
+	
+	print byte3, byte2, byte1, byte0
+	
+def byte_code_goto(val):
+	#opcode 20
+	pass
+	
+def byte_code_if(val1, val2, cond, goto):
+	'''
+	IF (val1 cond val2) GOTO goto
+	val1, val2, goto can be any val type
+	'''
+	#opcode starts with 32 
+	
+	#This inst owns the (0010-XXXX) address space
+	#0000 : ==
+	#0001 : !=
+	#0010 : >=
+	#0011 : <=
+	#0100 : <
+	#0101 : >
+	cond_code = {
+				'=='	: 0b0000,
+				'!='	: 0b0001,
+				'>='	: 0b0010,
+				'<='	: 0b0011,
+				'<'	: 0b0100,
+				'>'	: 0b0101
+	}
+	OPCODE = 0b0010 << 4
+	OPCODE |= cond_code[cond]
+	
+	byte2, byte1, byte0 = 0, 0, 0
+	byte7, byte6, byte5, byte4 = 0, 0, 0, 0
+	
+	#******************val1**********************
+	if val1.type == 'INT': 
+	#val one is a const
+		print "val1 : INT"
+		byte2 |= 0b00 << 6
+		byte0 = val1.val
+	
+	elif val1.any_var:
+	#val will be a variable; either V or Arr[C]
+		print "val1 : VAR"
+		byte2 |= 0b01 << 6
+		byte0 = get_var(val1)
+		
+	else:
+	#val is Ar[V]
+		print "val1 : VAR ARR"
+		byte2 |= 0b10 <<6
+		byte0 = pru_vars[val1.val[1]]
+		byte1 = pru_arrs[val1.val[0]][0]
+	
+	#******************val2**********************
+	if val2.type == 'INT': 
+	#val one is a const
+		print "val2 : INT"
+		byte2 |= 0b00 << 4
+		byte4 = val2.val
+	
+	elif val2.any_var:
+	#val will be a variable; either V or Arr[C]
+		print "val2 : VAR"
+		byte2 |= 0b01 << 4
+		byte4 = get_var(val2)
+		
+	else:
+	#val is Ar[V]
+		print "val2 : VAR ARR"
+		byte2 |= 0b10  << 4
+		byte4 = pru_vars[val2.val[1]]
+		byte5 = pru_arrs[val2.val[0]][0]
+		
+	#******************goto**********************
+	if goto.type == 'INT': 
+	#val one is a const
+		print "goto : INT"
+		byte2 |= 0b00 << 2
+		byte6 = goto.val
+	
+	elif goto.any_var:
+	#val will be a variable; either V or Arr[C]
+		print "goto : VAR"
+		byte2 |= 0b01 << 2
+		byte6 = get_var(goto)
+		
+	else:
+	#val is Ar[V]
+		print "goto : VAR ARR"
+		byte2 |= 0b10  << 2
+		byte6 = pru_vars[goto.val[1]]
+		byte7 = pru_arrs[goto.val[0]][0]
+	
+	byte3 = OPCODE
+	print byte3, byte2, byte1, byte0
+	print byte7, byte6, byte5, byte4		
+
+
+def byte_code_arithmetic(cmd, val1, val2):
+	'''
+	generates code for two operand arithmetic instructions
+	AND, SUB, MUL, DIV
+	BSR, BSL, AND, NOT
+	'''
+	#opcode starts from 48 (11-0000), ends at 63(11-1111)
+	#This inst owns the (0011-XXXX) address space
+	
+	opcode_dict = {
+					'ADD'	: 48,
+					'SUB'	: 50,
+					'MUL'	: 52,
+					'DIV'	: 54, 
+					'BSL'	: 56,
+					'BSR'	: 58, 
+					'AND'	: 60,
+					'NOT'	: 62
+	}
+	
+	OPCODE = opcode_dict[cmd]
+	#ADD x, y
+	#x, y : INT, VAR, ARR[Var]
+	
+	byte3, byte2, byte1, byte0 = 0,0,0,0
+	byte7, byte6, byte5, byte4 = 0,0,0,0
+	
+	if val1.arr_var or val2.arr_var:
+		#64 bit inst
+		OPCODE +=1
+		
+		if val1.arr_var:
+		#val1 is of type Arr[var]
+			print "val1 : ARR_VAR"
+			byte2 |= 0b10 << 6
+			byte0 = pru_vars[val1.val[1]]
+			byte1 = pru_arrs[val1.val[0]][0]
+			
+			if val2.type == 'INT': 
+			#val2 is a const
+				print "val2 : INT"
+				byte2 |= 0b00 << 4
+				byte0 = val2.val
+	
+			elif val2.any_var:
+			#val will be a variable; either V or Arr[C]
+				print "val2 : VAR"
+				byte2 |= 0b01 << 4
+				byte4 = get_var(val2)
+				
+			else :
+			#val2 Arr[var]
+				print "val2 : ARR_VAR"
+				byte2 |= 0b10 << 4
+				byte4 = pru_vars[val2.val[1]]
+				byte5 = pru_arrs[val2.val[0]][0]
+				
+		else:
+		#val1 is not arr[var]; val2 is.
+			if val1.type == 'INT': 
+			#val one is a const
+				print "val1 : INT"
+				byte2 |= 0b00 << 6
+				byte0 = val1.val
+				
+			else:
+			#val is a var
+				print "val1 : VAR"
+				byte2 |= 0b01 <<6
+				byte0 = get_var(val1)
+
+			#val2 stuff
+			byte2 |= 0b10 << 4
+			byte4 = pru_vars[val2.val[1]]
+			byte5 = pru_arrs[val2.val[0]][0]
+			
+	else:
+		#32 bit instr
+		#x, y : V/C
+		
+		#***********val1************
+		if val1.type == 'INT': 
+		#val one is a const
+			print "val1 : INT"
+			byte2 |= 0b00 << 7
+			byte1 = val1.val
+	
+		else:
+		#val will be a variable; either V or Arr[C]
+			print "val1 : VAR"
+			byte2 |= 0b01 << 7
+			byte1 = get_var(val1)
+			
+		#***********val2************
+		if val2.type == 'INT': 
+		#val one is a const
+			print "val2 : INT"
+			byte2 |= 0b00 << 6
+			byte0 = val2.val
+	
+		else:
+		#val will be a variable; either V or Arr[C]
+			print "val1 : VAR"
+			byte2 |= 0b01 << 6
+			byte0 = get_var(val2)
+
+	byte3= OPCODE
+	print byte3, byte2, byte1, byte0
+	print byte7, byte6, byte5, byte4
+	
+#control variables (like pre processor stuff or rather compiler directives, 
+#not to be compiled: (END)SCRIPT, RUN, ABORT, DEBUG, RESET - run by python compiler module
+
 """
 Grammar for the parser :
 
@@ -245,22 +506,32 @@ def p_inst_SET(p):
 def p_inst_WAIT(p):
 	'''inst : WAIT val'''
 	print "WAIT command - val :", p[2]
+	byte_code_single_op(p[1], p[2])
 	
 def p_inst_GOTO(p):
 	'''inst : GOTO val'''
 	print "GOTO command - val :", p[2]
+	byte_code_single_op(p[1], p[2])
+	
+def p_inst_GET(p):
+	'''inst : GET val'''
+	print "GET command - val :", p[2]
+	byte_code_single_op(p[1], p[2])
 
 def p_inst_IF(p):
 	'''inst : IF '(' val cond val ')' GOTO val'''
 	print "IF command - ", "val1 :", p[3], "val2 :", p[5], "cond :", p[4], "GOTO :", p[8]
+	byte_code_if(p[3], p[5], p[4], p[8])
 	
 def p_inst_ADD(p):
 	'''inst : ADD val ',' val'''
 	print "ADD command -", " val1 : ", p[2], " val2 : " , p[4]
+	byte_code_arithmetic(p[1], p[2], p[4])
 	
 def p_inst_SUB(p):
 	'''inst : SUB val ',' val'''
 	print "SUB command -", " val1 : ", p[2], " val2 : " , p[4]
+	byte_code_arithmetic(p[1], p[2], p[4])
 
 def p_inst_SCRIPT(p):
 	'''inst : SCRIPT'''
@@ -327,7 +598,7 @@ s = [
 	'ENDSCRIPT',
 	'GOTO arr3[4]'
 ]
-print parser.parse("SET arr1[var1], var2")
+print parser.parse("SET var22, arr2[var2]")
 #for inst in s :
 #	parser.parse(inst)
 #print result
