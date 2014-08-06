69538bc656a13b1f96ed5b034789f391661de5be
deepakkarki <deepak6k@gmail.com>
2014-08-06 18:28:19 +0530

    fifo interface is now working

---
+++ src/userspace_lib/bs_fifo.py
@@ -0,0 +1,30 @@
+from pru_speak import pru_speak
+import os.path
+import os
+
+PATH = "/dev/pruspeak"
+
+if os.path.exists(PATH):
+	os.remove(PATH)
+
+os.mkfifo(PATH)
+f_read = open(PATH, "r")
+
+while True:
+	#blocking wait for data from client
+	bs_code = f_read.read()
+
+	if not bs_code:
+		continue
+
+        try:
+                ret = pru_speak.execute_instruction(bs_code)
+                print ret 
+		#move this to 'cat' implementation later on. may need another thread for this.
+
+        except Exception as e:
+                print e
+
+
+f_read.close()
+
