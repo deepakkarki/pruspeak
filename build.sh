6636422ed12fb61d843dec0ca699fd81a8abda9c
Jason Kridner <jdk@ti.com>
2015-01-19 21:51:14 +0000

    build.sh: use make and make install only

--- build.sh
+++ build.sh
@@ -1,21 +1,19 @@
 #make driver
 cd src/drivers
 make
-cp pru_speak.ko ../../bin/
-make clean
+make install
 cd ../..
 
 #make pru_firmware
 cd src/pru-firmware
 make
-cp pru0_firmware ../../bin/
-cp pru1_firmware ../../bin/
+make install
 cd ../..
 
 #make dts
 cd src/dts
 make
-cp BB-PRUSPEAK-00A0.dtbo ../../bin/
+make install
 cd ../..
 
 
